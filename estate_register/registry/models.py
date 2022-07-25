779bd25fa010c48fccffb86b3358648ea3471e92
Alex <54545559+TreasureMaster@users.noreply.github.com>
2022-07-25 12:03:05 +0300

    [add]: материал здания

--- estate_register/registry/models.py
+++ estate_register/registry/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Material(models.Model):
+    """Материал здания"""
+    material = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name='Материал',
+    )
+
+    class Meta:
+        verbose_name = 'Материал здания'
+        verbose_name_plural = 'Материалы зданий'
+        ordering = ('id',)
